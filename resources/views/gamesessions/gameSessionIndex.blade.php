b5548beb4625fc5a70f752cbb5ee3e6d34f5d5fd
olivier <carrereolivier@yahoo.fr>
2019-01-01 21:20:46 +0100

    added buttons for future actions.
    -Big button to create gamesession added. No action implemented.
    -show, edit and delete buttons added for each row. No action implemented.

--- resources/views/gamesessions/gameSessionIndex.blade.php
+++ resources/views/gamesessions/gameSessionIndex.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-10">
+                <button type="button" class="btn btn-primary btn-lg">Créer une nouvelle partie</button>
                 <table class="table table-hover">
 
                     <thead>
@@ -13,24 +14,37 @@
                         <th scope="col">Jeu</th>
                         <th scope="col">Par</th>
                         <th scope="col">Créé le</th>
-                        <th>Actions</th>
+
+                        <th scope="col">Actions</th>
+
                     </tr>
                     </thead>
                     <tbody>
                     @foreach($gamesessions as $gamesession)
 
                         <tr>
-                            <td class="clickable" aria-expanded="false" aria-controls="gameDescription" data-target="#gameDescription-{{$gamesession->id}}" data-toggle="collapse"><i class="fa fa-plus" aria-hidden="true"></i></td>
+                            <td class="clickable" aria-expanded="false" aria-controls="gameDescription"
+                                data-target="#gameDescription-{{$gamesession->id}}" data-toggle="collapse"><i
+                                        class="fa fa-plus" aria-hidden="true"></i></td>
                             <td>{{$gamesession->title}}</td>
                             <td>{{$gamesession->game}}</td>
                             <td>{{$gamesession->getUserNames->name}}</td>
                             <td>{{$gamesession->created_at}}</td>
-                            <td>buttons</td>
+                            <td>
+                                <button type="button" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></button>
+
+                                <button type="button" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i>
+                                </button>
+
+                                <button type="button" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i>
+                                </button>
+                            </td>
 
                         </tr>
                         <tr>
                             <td colspan="5">
-                                <div id="gameDescription-{{$gamesession->id}}" class="collapse">{{$gamesession->description}}</div>
+                                <div id="gameDescription-{{$gamesession->id}}"
+                                     class="collapse">{{$gamesession->description}}</div>
                             </td>
 
                         </tr>
