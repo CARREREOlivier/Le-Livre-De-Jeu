16186b24e6e837373876a5055c6af6890126971e
olivier <carrereolivier@yahoo.fr>
2019-01-01 10:37:29 +0100

    link to project github set

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,7 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/Flefounet/Le-Livre-De-Jeu">GitHub</a>
                 </div>
             </div>
         </div>
