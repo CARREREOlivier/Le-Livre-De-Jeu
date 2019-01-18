b26b9092116cbdab1d48adedf185bef4bc6ea884
olivier <carrereolivier@yahoo.fr>
2019-01-18 14:46:43 +0100

    Turnorder.update controller created and implemented

--- app/Http/Controllers/TurnOrderController.php
+++ app/Http/Controllers/TurnOrderController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
@@ -9,95 +9,101 @@ use Illuminate\Http\Request;
 class TurnOrderController extends Controller
 {
 
-  /**
-   * Display a listing of the resource.
-   *
-   * @return Response
-   */
-  public function index()
-  {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
 
-  }
+    }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create()
-  {
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
 
-  }
+    }
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-   */
-  public function store(Request $request)
-  {
-    $turnOrder=new TurnOrder();
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $turnOrder = new TurnOrder();
 
-    $turnOrder->gameturn_id=$request->gameturn_id;
-    $turnOrder->user_id=Auth()->user()->id;
-    $turnOrder->message=$request->message;
+        $turnOrder->gameturn_id = $request->gameturn_id;
+        $turnOrder->user_id = Auth()->user()->id;
+        $turnOrder->message = $request->message;
 
-    $turnOrder->save();
+        $turnOrder->save();
 
-    return redirect()->back();
-
-
-  }
-
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function show($id)
-  {
-
-  }
-
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function edit($id)
-  {
-
-  }
-
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function update($id)
-  {
+        return redirect()->back();
 
 
+    }
 
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id)
-  {
-    TurnOrder::where('id',$id)->get()->each->delete(); //FIXME: strange... it seems to not recognize the id as primary key.
+    }
 
-    return redirect()->back();
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
 
-  }
-  
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        error_log("id received is $id");
+        $turnOrder = TurnOrder::find($id);
+        if (isset($turnOrder)) {
+            $turnOrder->message = $request->message;
+            $turnOrder->save();
+
+        } else{abort(404);}
+
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        TurnOrder::where('id', $id)->get()->each->delete(); //FIXME: strange... it seems to not recognize the id as primary key.
+
+        return redirect()->back();
+
+    }
+
 }
 
 ?>
