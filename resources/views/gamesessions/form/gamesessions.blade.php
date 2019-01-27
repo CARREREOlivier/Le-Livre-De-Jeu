d5163545a6803f63fa009dd97c5b5411622b4543
olivier <carrereolivier@yahoo.fr>
2019-01-27 17:34:21 +0100

    css applied to gameSessionsNew.blade

--- resources/views/gamesessions/form/gamesessions.blade.php
+++ resources/views/gamesessions/form/gamesessions.blade.php
@@ -12,37 +12,44 @@
 
 {!! Form::open(array('route' => 'gamesession.store', 'method' => 'POST')) !!}
 {!! csrf_field() !!}
-<div class="container">
-    <div class="row">
-        <div class="col-lg-3">
-            {!! Form::label('title', 'Titre de la partie (obligatoire):') !!}
-        </div>
+<div class="container create-game">
+    <div class="row row-title"><h2 class="title big h2-black">Créer une partie</h2></div>
+    <div class="row strip">
+        <div class="vignette green-bg">
+            <div class="row">
+                <div class="col-lg-3">
+                    {!! Form::label('title', 'Titre de la partie (obligatoire):',['id'=>'title-label']) !!}
+                </div>
 
-        <div class="col-lg-9">
-           {!! Form::text('title','', array('required'=>'required', 'class'=>'gameSessionTitleInput')) !!}
-        </div>
+                <div class="col-lg-9">
+                    {!! Form::text('title','', array('required'=>'required', 'class'=>'gameSessionTitleInput')) !!}
+                </div>
 
-    </div>
-    <div class="row">
-        <div class="col-lg-3">
-            {!! Form::label('game', 'Jeu (optionnel):') !!}
+            </div>
+
+            <div class="row">
+                <div class="col-lg-3">
+                    {!! Form::label('game', 'Jeu (obligatoire):',['id'=>'game-label']) !!}
+                </div>
+                <div class="col-lg-9">
+                    {!! Form::text('game',null,['class'=>'gameInput']) !!}
+                </div>
+            </div>
         </div>
-        <div class="col-lg-9">
-            {!! Form::text('game') !!}
+    </div>
+
+    <div class="row strip">
+        <div class="vignette orange-bg">
+            <div class="col-lg">{!! Form::label('description', 'Description (optionnel):',["id"=>"description-label"]) !!}{!! Form::textarea('description') !!}</div>
         </div>
     </div>
-    <hr>
-    <div class="row">
-        <div class="col-lg"> {!! Form::label('description', 'Description (optionnel):') !!}</div>
-    </div>
-    <div class="row">
-        <div class="col-lg">{!! Form::textarea('description') !!}</div>
-    </div>
-    <hr>
+
+
     <div class="row">
         <div class="col-lg">@include('gamesessions.modals.modalAddPlayers')</div>
-        <div class="col-lg">{!! Form::submit('Créer', array('class'=>'btn btn-primary')) !!}</div>
+        <div class="col-lg">{!! Form::submit('Créer', array('class'=>'btn btn-secondary lined thin')) !!}</div>
     </div>
+
 </div>
 
 {!! Form::close() !!}
