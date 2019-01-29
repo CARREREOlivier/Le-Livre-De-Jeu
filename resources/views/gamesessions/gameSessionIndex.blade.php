1ab89a8f23ec89d21d2f1f6d7a0cce34afc18b39
olivier <carrereolivier@yahoo.fr>
2019-01-29 19:08:58 +0100

    time fo creation in gamesessionindex set to 24hou format

--- resources/views/gamesessions/gameSessionIndex.blade.php
+++ resources/views/gamesessions/gameSessionIndex.blade.php
@@ -28,7 +28,7 @@
 
                             <h3 class="welcome-card-title yellow">{{$gameSession->title}}
                             </h3>
-                            <p>Créée le:{{date('d-M-Y à h:i', strtotime($gameSession->created_at))}}</p>
+                            <p>Créée le:{{date('d-M-Y à H:i', strtotime($gameSession->created_at))}}</p>
                             <p>Avec : {{$gameSession->getUserNames->name}}</p>
                             <a href="{{route('gamesession.show', $gameSession->slug)}}"
                                class="btn btn-primary lined thin">Lire</a>
