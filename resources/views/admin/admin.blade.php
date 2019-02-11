9042504684a999a4389ecb23e6cf4a2a876679eb
olivier <carrereolivier@yahoo.fr>
2019-02-11 10:14:32 +0100

    redirect links added to admin view

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mt-5 mb-5">
-    @if( Route::currentRouteName()=='admin.main')
-        @include('admin.content.main')
-    @endif
-    </div>
+    @auth
+        @if(Auth::user()->status == 'Admin')
+        <div class="container mt-5 mb-5">
 
+                @if( Route::currentRouteName()=='admin.main')
+                    @include('admin.content.main')
+                @endif
+
+        </div>
+
+        @else {{ Redirect::to('/dashboard') }}
+
+        @endif
+    @endauth
+    @guest
+        
+        {{ Redirect::to('/') }}
+
+    @endguest
 @endsection
