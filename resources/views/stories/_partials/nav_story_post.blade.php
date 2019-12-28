e485a849ad082452eb624fdded9ff0f7599bfad7
olivier <carrereolivier@yahoo.fr>
2019-12-28 17:32:10 +0100

    CSS for nvertical nav in show_post.blade added

--- resources/views/stories/_partials/nav_story_post.blade.php
+++ resources/views/stories/_partials/nav_story_post.blade.php
@@ -1,5 +1,7 @@
-<ul>
-    @foreach ($allPosts as $post)
-        <li> <a href="{{route('story.show.post',$post->slug)}}">{{$post->title}}</a></li>
-    @endforeach
-</ul>
+<nav class="vertical">
+    <ul>
+        @foreach ($allPosts as $post)
+            <li><a href="{{route('story.show.post',$post->slug)}}">{{$post->title}}</a></li>
+        @endforeach
+    </ul>
+</nav>
