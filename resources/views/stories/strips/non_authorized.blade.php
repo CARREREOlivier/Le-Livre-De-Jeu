ae213a504abc46402642a5f86809741e9639448a
olivier <carrereolivier@yahoo.fr>
2020-03-01 11:33:53 +0100

    modified styles and minor visual modifications

--- resources/views/stories/strips/non_authorized.blade.php
+++ resources/views/stories/strips/non_authorized.blade.php
@@ -1,1 +1,5 @@
-<p> Vous n'êtes pas autorisé à accéder à cette page. </p>
+<div class="row strip white-bg">
+    <div class="col-md-12 vignette green-bg">
+        <p> Vous n'êtes pas autorisé à accéder à cette page. </p>
+    </div>
+</div>
--- resources/views/stories/strips/show_table_of_contents.blade.php
+++ resources/views/stories/strips/show_table_of_contents.blade.php
@@ -10,17 +10,26 @@
             @else
 
                 @foreach($posts as $post)
-                    <tr>
-                        @include('stories._partials.table_of_content_actions')
 
-                        <td>  {!! $post->title !!} par {{$post->author}}</td>
-                        @if($post->co_author !== 'none')
-                            <td> et {{$post->co_author}}</td>
-                        @endif
-                    </tr>
+                    @if($post->isVisible === true)
+                        <tr style="font-family: 'Patrick Hand SC'">
+                            @include('stories._partials.table_of_content_actions')
+
+                            <td style="font-size: 20px;; font-style: italic;">{!! $post->title !!}</td>
+                            <td>&nbsp;&nbsp;par&nbsp;</td>
+                            <td style="font-style: italic;"> {{$post->authorName}}</td>
+                            @if($post->co_author !== 'none')
+                                <td>&nbsp;&nbsp;et&nbsp;&nbsp;</td>
+                                <td style="font-style: italic;">{{$post->co_authorsNames}}</td>
+                            @endif
+                        </tr>
+                    @endif
+
+
                 @endforeach
 
             @endif
         </table>
     </div>
+</div>
 </div>
